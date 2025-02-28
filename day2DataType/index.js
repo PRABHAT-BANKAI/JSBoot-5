ebb4ed7281467b86b427b9add02e06ecf999b495
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-02-28 17:55:03 +0530

    datatypes

---
+++ day2DataType/index.js
@@ -0,0 +1,108 @@
+// what is variables
+// understanding data types
+// Types Of Operator
+// Arithmetic operators
+// Assignment operators
+// =
+// +=
+// -=
+// *=
+// /=
+// **=
+// %=
+// Comparison operators
+//==
+//===
+//>=
+// <=
+//!=
+//!==
+//>
+//<
+// Logical operators
+//  &&   , ||
+// conversion with console.log and innerHTML
+
+let userName = "batman";
+
+// name is a variable = variable is store value
+
+console.log(userName);
+
+// data types
+// Primitive data types
+// String "batman" `batman` 'batman'
+// Number 24
+// Boolean tru false
+// BigInt
+// undefined
+// null
+
+// non-primitive data types
+// Object
+// Array
+// function
+
+let number = 12343253464575676768678789n;
+
+console.log(number);
+
+let string = "batman";
+let num = 12;
+let boolean = true;
+let temp = null;
+
+let object = {
+  hero: "hulk",
+  age: 12,
+  power: 1000,
+}; // ojbect create key value pair
+
+console.log(object);
+
+let array = [1, 2, 3, "hero", true];
+
+print();
+// normal function
+function print() {
+  console.log("hi from function");
+}
+
+print();
+
+// Arithmetic operators
+// + adding
+// - subtracting
+// % modules return reminder
+// /  divide   return quotent
+// *  multiplication
+// ** exponent (power)
+
+let a = 11;
+let b = 2;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a % b);
+
+console.log(a / b);
+console.log(Math.floor(a / b));
+
+console.log(a * b);
+
+console.log(a ** b);
+
+let sum = 0;
+
+let c = 15;
+
+let d = 10;
+
+sum = sum + c;
+
+sum += c;
+
+
+console.log(4==="4")
