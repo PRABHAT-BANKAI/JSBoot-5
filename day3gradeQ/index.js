5c36e7851d03d9dde5bbb5ee3cda02b5287c5499
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-04 17:25:32 +0530

    question

---
+++ day3gradeQ/index.js
@@ -0,0 +1,26 @@
+// Some Logical Programs
+// swapping
+// Ex : grade , bill , interest , employ salary
+
+let temp = 0;
+
+let a = 10;
+
+let b = 5;
+// temp = a;
+
+// a = b;
+// b = temp;
+
+a = a+b
+b = a-b
+a = a-b
+
+
+let english = 100
+let hindi = 250
+let math = 600
+
+let average = (english+hindi+math)/3
+console.log("Average=>", average)
+
