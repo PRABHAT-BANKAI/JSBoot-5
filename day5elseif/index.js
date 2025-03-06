c573a9d5eb756ed31796af9bfa92a7e0c4ea9f47
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-06 17:15:15 +0530

    javascript

---
+++ day5elseif/index.js
@@ -0,0 +1,35 @@
+// else if ladder Statement
+// Choice else if ladder Statement
+// Range Program with Logical operators
+
+let light = "yellow";
+
+if (light === "red") {
+  console.log("Stop");
+} else if (light == "yellow") {
+  console.log("ready ");
+} else if (light == "yellow") {
+  console.log("Slow Down");
+} else if (light == "green") {
+  console.log("Go");
+} else {
+  console.log("Invalid color");
+}
+
+
+
+let percentageofStudent = 55
+if(percentageofStudent>90 && percentageofStudent<100){
+  console.log("A");
+}else if( percentageofStudent >80 && percentageofStudent<90){
+  console.log("B");
+}
+else if( percentageofStudent >70 && percentageofStudent<80){
+  console.log("B");
+}
+else if( percentageofStudent >60 && percentageofStudent<70){
+  console.log("B");
+}
+else{
+  console.log("F");
+}
