2024ce8b9cd8b2d2e024d77971aca03c2afad312
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-11 17:03:37 +0530

    typesofFunction

---
+++ day8functiontype/index.js
@@ -0,0 +1,76 @@
+// Function with four ways
+// Recursion
+// Nested Functions
+
+// variables as a Function expression
+// arrow Function
+
+// normal function
+// variable function
+// arrow function
+// anonymous function
+
+// console.log(sum(1, 2));
+
+// variable function cannot get before initialization
+let sum = function (a, b) {
+  return a + b;
+}; // anonymous function
+
+let store = sum(12, 5);
+console.log(store);
+
+// arrow function
+//syntax
+
+//  ()=>{}
+
+let subtract = (a, b) => a - b;
+
+console.log(subtract(55, 2));
+
+//5
+//4
+//3
+//2
+//1
+
+function order(num) {
+  if (num == 0) {
+    return;
+  }
+  console.log(num);
+
+  order(num - 1);
+}
+
+order(5);
+
+// nested Function
+function outerFun() {
+  // functional scope
+  console.log("outer function");
+
+  function innerFun() {
+    console.log("inner function");
+  }
+
+  return innerFun;
+}
+
+// outerFun();
+
+let storeInner = outerFun();
+
+storeInner();
+
+
+
+// IIFE(Immediately Invoked Function Expression )
+
+(function () {
+  console.log("IIFE");
+})();
+
+
+// Closure concepts will continue tomorrow 
