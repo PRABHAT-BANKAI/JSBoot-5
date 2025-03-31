feea2d674a6ed83f4ab44a6df316cee8ff4b4f68
Prabhat-kumar-11 <prabhssgg3@gmail.com>
2025-03-31 14:17:13 +0530

    selectors

---
+++ outputQuestion/index.js
@@ -0,0 +1,16 @@
+// for (let i = 1; i < 7; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   });
+// }
+
+
+// let array = [];
+
+// for(var i=0;i<5;i++){
+//   arr[i]=function(){
+//     return i
+//   }
+// }
+
+// console.log(array[4])
